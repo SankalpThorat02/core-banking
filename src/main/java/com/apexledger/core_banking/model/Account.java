5b3e42c9f1e4ab299fc624cb2a6aa904afb3d506
SankalpThorat02 <sankalpt2225@gmail.com>
2026-05-21 17:13:51 +0530

    account entity

---
+++ src/main/java/com/apexledger/core_banking/model/Account.java
@@ -0,0 +1,29 @@
+package com.apexledger.core_banking.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "sankalp_account")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    @Column(unique = true, nullable = false)
+    private String accountNumber;
+
+    @Column(nullable = false)
+    private String currency;        // 'INR' , 'USD'
+
+    @Column(nullable = false)
+    private String status;          //'Active' , 'Frozen'
+}
---
+++ src/main/java/com/apexledger/core_banking/model/Transaction.java
@@ -0,0 +1,8 @@
+package com.apexledger.core_banking.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table()
+public class Transaction {
+}
