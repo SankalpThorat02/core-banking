0a88a15949fe1b4bda70e1364b23dda82f4c1be9
SankalpThorat02 <sankalpt2225@gmail.com>
2026-05-22 12:01:04 +0530

    feat: implement transfer funds service

---
+++ src/main/java/com/apexledger/core_banking/service/LedgerService.java
@@ -0,0 +1,63 @@
+package com.apexledger.core_banking.service;
+
+import com.apexledger.core_banking.model.Account;
+import com.apexledger.core_banking.model.Transaction;
+import com.apexledger.core_banking.repository.AccountRepository;
+import com.apexledger.core_banking.repository.TransactionRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Service
+public class LedgerService {
+
+    private final AccountRepository accountRepository;
+    private final TransactionRepository transactionRepository;
+
+    public LedgerService(AccountRepository accountRepository, TransactionRepository transactionRepository) {
+        this.accountRepository = accountRepository;
+        this.transactionRepository = transactionRepository;
+    }
+
+    @Transactional
+    public Transaction transferFunds(String fromAccount, String toAccount, BigDecimal amount, String idempotencyKey) {
+
+        if(transactionRepository.existsByIdempotencyKey(idempotencyKey)) {
+            throw new RuntimeException("Duplicate transaction request detected !");
+        }
+
+        Account sourceAccount = accountRepository.findByAccountNumber(fromAccount)
+                .orElseThrow(() -> new RuntimeException("Source account not found"));
+
+        Account destinationAccount = accountRepository.findByAccountNumber(toAccount)
+                .orElseThrow(() -> new RuntimeException("Destination account not found"));
+
+        if(!sourceAccount.getStatus().equals("ACTIVE") || !destinationAccount.getStatus().equals("ACTIVE")) {
+            throw new RuntimeException("One or both accounts are not active");
+        }
+
+        BigDecimal totalCredits = transactionRepository.calculateTotalCredits(sourceAccount);
+        BigDecimal totalDebits = transactionRepository.calculateTotalDebits(sourceAccount);
+        BigDecimal currentBalance = totalCredits.subtract(totalDebits);
+
+        if(currentBalance.compareTo(amount) < 0) {
+            throw new RuntimeException("Insufficent funds");
+        }
+
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        transaction.setSourceAccount(sourceAccount);
+        transaction.setDestinationAccount(destinationAccount);
+        transaction.setTimestamp(LocalDateTime.now());
+        transaction.setIdempotencyKey(idempotencyKey);
+        transaction.setStatus("COMPLETED");
+
+        transactionRepository.save(transaction);
+    }
+}
