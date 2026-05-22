7fea6379f70a6ed95fe18f389b8df8fada33be49
SankalpThorat02 <sankalpt2225@gmail.com>
2026-05-22 11:33:10 +0530

    implement account and Transaction repository

---
+++ src/main/java/com/apexledger/core_banking/repository/TransactionRepository.java
@@ -0,0 +1,23 @@
+package com.apexledger.core_banking.repository;
+
+import com.apexledger.core_banking.model.Account;
+import com.apexledger.core_banking.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.servlet.tags.form.SelectTag;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    boolean existsByIdempotencyKey (String idempotencyKey);
+
+    //Total Money credited
+    @Query("SELECT COALESCE(SUM (t.amount), 0) FROM Transaction t WHERE t.destinationAccount = :account AND t.status = 'COMPLETED' ")
+    BigDecimal calculateTotalCredits(@Param("account") Account account);
+
+    //Total Money debited
+    @Query("SELECT COALESCE(SUM (t.amount), 0) FROM Transaction t WHERE t.sourceAccount = :account AND t.status = 'COMPLETED' ")
+    BigDecimal calculateTotalDebits(@Param("account") Account account);
+}
