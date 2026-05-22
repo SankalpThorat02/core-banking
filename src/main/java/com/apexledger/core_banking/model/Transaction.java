308c60731555977674943a37702baa70eb966807
SankalpThorat02 <sankalpt2225@gmail.com>
2026-05-22 11:14:37 +0530

    transaction entity

--- src/main/java/com/apexledger/core_banking/model/Transaction.java
+++ src/main/java/com/apexledger/core_banking/model/Transaction.java
@@ -1,8 +1,42 @@
 package com.apexledger.core_banking.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
-@Table()
+@Table(name = "sankalp_transactions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    @ManyToOne
+    @JoinColumn(name = "source_accout_id", nullable = false)
+    private Account sourceAccount;
+
+    @ManyToOne
+    @JoinColumn(name = "destination_account_id", nullable = false)
+    private Account destinationAccount;
+
+    @Column(nullable = false, precision = 19, scale = 4)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private String status;              // PENDING, COMPLETED, FAILED
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(unique = true, nullable = false)
+    private String idempotencyKey;
 }
